b9dd80efb3ee073de06453664b6233b4107371f5
Jefferson <jefferson.rubior@uqvirtual.edu.co>
2025-11-13 23:25:40 -0500

    cambio para el fronted

--- src/main/java/com/example/restaurapp/config/CorsConfig.java
+++ src/main/java/com/example/restaurapp/config/CorsConfig.java
@@ -12,9 +12,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // permite todos los endpoints
-                        .allowedOrigins("http://localhost:4200",
-                                "http://4.155.250.187",
-                                "http://4.155.250.187/") // tu frontend
+                        .allowedOrigins("*") // tu frontend
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
